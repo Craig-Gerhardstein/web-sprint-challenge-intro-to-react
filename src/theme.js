1c89ea4bffb19e7e5c20b725e5401f4ae143e9a8
Craig Gerhardstein <cgerhardstein@live.com>
2021-02-19 05:52:46 -0500

    Just have to answer questions

---
+++ src/theme.js
@@ -0,0 +1,14 @@
+export default {
+    firstColor: "#4e3d91",
+    secondColor: "goldenrod",
+    green: "limegreen",
+    blue: "blue",
+    black: "#202020",
+    white: "white",
+    light: "lightgrey",
+
+    breakpoints: {
+        mobile: "(max-width: 550px)",
+        tablet: "(max-width: 800px)",
+    }
+} 
